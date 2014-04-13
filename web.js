01e04fbe21a591dced6845877be21fc3c3ace434
clindsay107 <clindsay107@gmail.com>
2014-04-13 23:39:42 +0000

    routes defined

--- web.js
+++ web.js
@@ -12,8 +12,53 @@ var Todo = mongoose.model('Todo', {
 
 //routes
 
-app.get('/', function(req, res){
+//expose all of our TODOs
+app.get('/api/todos', function(req, res) {
+  Todo.find(function(err, todos) {
+    if (err)
+      res.send(err);
+    
+    res.json(todos);
+  });
+});
+
+//create a TODO item
+app.post('/api/todos', function(req, res) {
+  Todo.create({
+    text: req.body.text,
+    done: false
+  }, function(err, todo) {
+    if (err)
+      res.send(err);
+      
+    //refresh our list of TODOs on the creation of a new one
+    Todo.find(function(err, todos) {
+      if (err)
+        res.send(err);
+      res.json(todos);
+    });
+  });
+});
+
+//delete a TODO item
+
+app.delete('/api/todos/:todo_id', function(req, res) {
+  Todo.remove({
+    _id: req.params.todo_id
+  }, function(err, todo) {
+    if (err)
+      res.send(err);
+    
+    Todo.find(function(err, todos) {
+      if (err)
+        res.send(err);
+      res.json(todos);
+    });
+  });
+});
+
+app.get('/', function(req, res) {
   res.send('hello world');
 });
 
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 3000, "0.0.0.0");
