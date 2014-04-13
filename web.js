bd5b1a787f16f3832717db3d541dd3055381bf17
clindsay107 <clindsay107@gmail.com>
2014-04-13 22:38:31 +0000

    basic setup with host binding for nitrous.io preview. Using mongoHQ for db hosting

--- web.js
+++ web.js
@@ -2,4 +2,10 @@ var express = require("express");
 var app = express();
 var mongoose = require("mongoose");
 
-mongoose.connect();
+mongoose.connect("mongodb://clindsay107:password123@oceanic.mongohq.com:10024/app24104304");
+
+app.get('/', function(req, res){
+  res.send('hello world');
+});
+
+app.listen(3000, "0.0.0.0");
